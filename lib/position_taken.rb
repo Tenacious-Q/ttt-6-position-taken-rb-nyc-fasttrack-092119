6d44092f284753b505fbef8feacb1ce41d25dcc7
Michael Quigley <mikequigley@quigleypublishing.com>
2020-03-07 05:52:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,8 +3,7 @@
 
 def position_taken?(board, index)
 
-
-   !board[index] == " "  # the board index value is not an empty space
+   board[index] != " "  # the board index value is not an empty space
 
    board[index] == "X" || board[index] == "O"
 
